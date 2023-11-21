ec277b09b29d99a606fe6224f019782b3e5d5f03
Villa25698 <bardha@hiof.no>
2023-11-21 10:16:23 +0100

    fjernet lag tjenester knappen på alle tjenester

--- src/components/AllServices.vue
+++ src/components/AllServices.vue
@@ -1,120 +1,112 @@
 <template>
-    <div class="services-box">
-      <h2 class="services-title">All Services ({{ services.length }})</h2>
-      <div class="button-container">
-        <button class="modern-button" @click="navigateToServiceForm">Make Services</button>
-      </div>
-      <div v-if="services.length === 0">Loading services...</div>
-      <div v-else class="service-container">
-        <div v-for="service in services" :key="service.uid" class="service-card">
-          <router-link :to="`/${formatServiceNameToURL(service.uid)}`">
-            <h3>{{ service.serviceName }}</h3>
-            <img :src="`http://localhost:8080/images/${service.uid}.png`" alt="Service Image" width="250" height="250">
-            <p>{{ service.description }}</p>
-            <p>Location: {{ service.location }}</p>
-            <p>Date: {{ service.date }}</p>
-            <p>Price: {{ service.price }}</p>
-          </router-link>
-          <button v-if="accountDetails.username === 'Admin' || service.account === accountDetails.username" class="delete-button" @click="confirmDelete(service.uid)">Delete</button>
-        </div>
+  <div class="services-box">
+    <h2 class="services-title">All Services ({{ services.length }})</h2>
+    <div v-if="services.length === 0">Loading services...</div>
+    <div v-else class="service-container">
+      <div v-for="service in services" :key="service.uid" class="service-card">
+        <router-link :to="`/${formatServiceNameToURL(service.uid)}`">
+          <h3>{{ service.serviceName }}</h3>
+          <img :src="`http://localhost:8080/images/${service.uid}.png`" alt="Service Image" width="250" height="250">
+          <p>{{ service.description }}</p>
+          <p>Location: {{ service.location }}</p>
+          <p>Date: {{ service.date }}</p>
+          <p>Price: {{ service.price }}</p>
+        </router-link>
+        <button v-if="accountDetails.username === 'Admin' || service.account === accountDetails.username" class="delete-button" @click="confirmDelete(service.uid)">Delete</button>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'AccountServices',
-    data() {
-      return {
-        services: [],
-        accountDetails: null,
-      };
+<script>
+export default {
+  name: 'AccountServices',
+  data() {
+    return {
+      services: [],
+      accountDetails: null,
+    };
+  },
+  methods: {
+    formatServiceNameToURL(serviceName) {
+      return serviceName.toLowerCase().replace(/\s+/g, '-');
     },
-    methods: {
-      navigateToServiceForm() {
-        this.$router.push({ name: 'ServiceFormPage' });
-      },
-  
-      formatServiceNameToURL(serviceName) {
-        return serviceName.toLowerCase().replace(/\s+/g, '-');
-      },
-  
-      fetchServiceImages() {
-        this.services.forEach(service => {
-          fetch(`http://localhost:8080/images/${service.uid}.png`)
-            .then(response => {
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-              return response.blob();
-            })
-            .then(imageBlob => {
-              service.imageUrl = URL.createObjectURL(imageBlob);
-            })
-            .catch(error => {
-              console.error('Error fetching image:', error);
-              service.imageUrl = ''; // Handle missing image case
-            });
-        });
-      },
-  
-      confirmDelete(serviceId) {
-        if (confirm("Are you sure you want to delete this service?")) {
-          this.deleteService(serviceId);
-        }
-      },
-  
-      deleteService(serviceId) {
-        let apiUrl;
-        if (this.accountDetails.username === 'Admin') {
-          apiUrl = `http://localhost:8080/admin/services/${serviceId}`;
-        } else {
-          apiUrl = `http://localhost:8080/user/services/${this.accountDetails.username}/${serviceId}`;
-        }
-  
-        fetch(apiUrl, {
-          method: 'DELETE',
-        })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          window.location.reload(); // Refresh the services list after deletion
-        })
-        .catch(error => {
-          console.error('Error deleting service:', error);
-        });
-      },
-  
-      fetchServices() {
-        fetch('http://localhost:8080/advertisement/services/all')
+
+    fetchServiceImages() {
+      this.services.forEach(service => {
+        fetch(`http://localhost:8080/images/${service.uid}.png`)
           .then(response => {
             if (!response.ok) {
               throw new Error('Network response was not ok');
             }
-            return response.json();
+            return response.blob();
           })
-          .then(data => {
-            this.services = data;
-            this.fetchServiceImages();
+          .then(imageBlob => {
+            service.imageUrl = URL.createObjectURL(imageBlob);
           })
           .catch(error => {
-            console.error('Error fetching services:', error);
+            console.error('Error fetching image:', error);
+            service.imageUrl = ''; // Handle missing image case
           });
-      },
+      });
     },
-  
-    mounted() {
-      const storedAccount = localStorage.getItem('accountDetails');
-      if (storedAccount) {
-        this.accountDetails = JSON.parse(storedAccount);
+
+    confirmDelete(serviceId) {
+      if (confirm("Are you sure you want to delete this service?")) {
+        this.deleteService(serviceId);
       }
-  
-      this.fetchServices();
+    },
+
+    deleteService(serviceId) {
+      let apiUrl;
+      if (this.accountDetails.username === 'Admin') {
+        apiUrl = `http://localhost:8080/admin/services/${serviceId}`;
+      } else {
+        apiUrl = `http://localhost:8080/user/services/${this.accountDetails.username}/${serviceId}`;
+      }
+
+      fetch(apiUrl, {
+        method: 'DELETE',
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        window.location.reload(); // Refresh the services list after deletion
+      })
+      .catch(error => {
+        console.error('Error deleting service:', error);
+      });
+    },
+
+    fetchServices() {
+      fetch('http://localhost:8080/advertisement/services/all')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          this.services = data;
+          this.fetchServiceImages();
+        })
+        .catch(error => {
+          console.error('Error fetching services:', error);
+        });
+    },
+  },
+
+  mounted() {
+    const storedAccount = localStorage.getItem('accountDetails');
+    if (storedAccount) {
+      this.accountDetails = JSON.parse(storedAccount);
     }
-  };
-  </script>
-  
+
+    this.fetchServices();
+  }
+};
+</script>
  
   
   
